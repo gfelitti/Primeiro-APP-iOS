120ba72202b122ccae78228c15783f2f6f540d9b
guilherme felitti <gfelitti@gmail.com>
2014-10-11 16:50:48 -0300

    primeiro app

---
+++ VIEW2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  VIEW2ViewController.h
+//  Primeira
+//
+//  Created by guilherme felitti on 16/07/14.
+//  Copyright (c) 2014 Guilherme Felitti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VIEW2ViewController : UIViewController
+
+@end
---
+++ VIEW2ViewController.m
@@ -0,0 +1,49 @@
+//
+//  VIEW2ViewController.m
+//  Primeira
+//
+//  Created by guilherme felitti on 16/07/14.
+//  Copyright (c) 2014 Guilherme Felitti. All rights reserved.
+//
+
+#import "VIEW2ViewController.h"
+
+@interface VIEW2ViewController ()
+
+@end
+
+@implementation VIEW2ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
